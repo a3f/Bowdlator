b1dd16a9a8f727151d32696fe01db9b691ce351c
Ahmad Fatoum <ahmad@a3f.at>
2016-08-17 16:35:05 +0200

    Add install script

---
+++ install.sh
@@ -0,0 +1,3 @@
+BUILD_DIR="$HOME/Library/Developer/Xcode/DerivedData/ParrotIM-acnrgnrpyhpekdestynfucvauwzn"
+cp -r "$BUILD_DIR/Build/Products/Debug/Parrot.app" $HOME/Library/Input\ Methods/
+pkill Parrot
